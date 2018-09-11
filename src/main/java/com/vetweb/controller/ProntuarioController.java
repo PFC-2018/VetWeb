93f7993a4e413c89436888060a8bce58100fd984
murillo <murillo@pezzuol-2.local>
2018-09-11 18:19:51 -0300

    Tela PRONTUARIO2 para teste.

--- src/main/java/com/vetweb/controller/ProntuarioController.java
+++ src/main/java/com/vetweb/controller/ProntuarioController.java
@@ -166,7 +166,7 @@ public class ProntuarioController {
     @RequestMapping(value = "/prontuarioDoAnimal/{animalId}", method = RequestMethod.GET)
     public ModelAndView prontuarioDoAnimal(@PathVariable("animalId") final Long animalId, @ModelAttribute("atendimento") OcorrenciaAtendimento atendimento,
     		@ModelAttribute("prontuarioPatologia") Patologia patologia, @ModelAttribute("prontuarioVacina") Vacina vacina) {
-        ModelAndView modelAndView = new ModelAndView("prontuario/prontuario");
+        ModelAndView modelAndView = new ModelAndView("prontuario/prontuario2");
         Prontuario prontuario = prontuarioDAO.buscarProntuarioPorAnimal(animalId);
 		modelAndView.addObject("prontuario", prontuario);
 		modelAndView.addObject("tiposDeAtendimento", atendimentoDAO.buscarTiposDeAtendimento());
