77ae18a9387c16fd16836f7b7b7ed45e5086dd42
mupezzuol <murillopezzuol@hotmail.com>
2018-11-15 17:12:24 -0200

    ajutes.

--- src/main/java/com/vetweb/service/JasperService.java
+++ src/main/java/com/vetweb/service/JasperService.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
